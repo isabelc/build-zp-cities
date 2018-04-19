ec2550c87b2b880e3fd36109b5783bc0f6ecc642
Isabel <isabel@isabelcastillo.com>
2018-04-18 21:10:07 -0400

    add file header comment block

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+/**
+ * ZodiacPress cities.txt Builder
+ *
+ * This file is part of "ZodiacPress cities.txt Builder".
+ * 
+ * "ZodiacPress cities.txt Builder" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ * 
+ * "ZodiacPress cities.txt Builder" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with "ZodiacPress cities.txt Builder". If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author     Isabel Castillo
+ * @copyright  2018 Isabel Castillo
+ * @license    https://www.gnu.org/licenses/gpl-2.0.html  GNU GPLv2
+ */
+?>
 <!DOCTYPE html>
 <html>
 <head>
